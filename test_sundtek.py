f84d9bc6be23a4f10b87e0bf4c10ae64208a152e
Alexander Grothe <sehawk1986@gmx.de>
2019-07-28 16:06:06 +0200

    check for hardware decoder before trying to change ir protocol

--- test_sundtek.py
+++ test_sundtek.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 import sundtek_api
 
 try:
@@ -13,12 +14,18 @@ else:
     print(devices)
 
     for device, data in devices.items():
-        print("check device", device, data)
         if data.get('frontend_node') and data.get('remote_node'):
-            for ir_protocol in data.get('ir_protocols').keys():
-                print(f"set protocol for {data.get('frontend_node')} to {ir_protocol}")
-                sundtek_api.set_ir_protocol(data.get('frontend_node'), ir_protocol)
-                assert sundtek_api.ir_protocols(data.get('frontend_node')).get('active_ir_protocol') == ir_protocol
+            for ir_protocol, description in data.get('ir_protocols').items():
+                if not "hardware decoder" in description:
+                    print(f"trying set protocol for {data.get('frontend_node')} to {ir_protocol}")
+                    try:
+                        sundtek_api.set_ir_protocol(data.get('frontend_node'), ir_protocol)
+                    except ValueError as e:
+                        print(e, file=sys.stderr)
+                        print("More recent sundtek devices only support a NEC hardware decoder.")
+                    else:
+                        if not sundtek_api.ir_protocols(data.get('frontend_node')).get('active_ir_protocol') == ir_protocol:
+                            print(f"Could not set protocol to {description} ({ir_protocol})", file=sys.stderr)
 
 print("Network Devices:")
 print(sundtek_api.network_devices())
