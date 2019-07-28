c9fdeb189546821f8f0f2855728c43637003767c
Alexander Grothe <seahawk1986@gmx.de>
2019-07-28 15:57:17 +0200

    update test script

--- test_sundtek.py
+++ test_sundtek.py
@@ -15,8 +15,10 @@ else:
     for device, data in devices.items():
         print("check device", device, data)
         if data.get('frontend_node') and data.get('remote_node'):
-            print(f"set protocol for {data.get('frontend_node')} to NEC")
-            sundtek_api.set_ir_protocol(data.get('frontend_node'), sundtek_api.IR_PROTO_NEC)
+            for ir_protocol in data.get('ir_protocols').keys():
+                print(f"set protocol for {data.get('frontend_node')} to {ir_protocol}")
+                sundtek_api.set_ir_protocol(data.get('frontend_node'), ir_protocol)
+                assert sundtek_api.ir_protocols(data.get('frontend_node')).get('active_ir_protocol') == ir_protocol
 
 print("Network Devices:")
 print(sundtek_api.network_devices())
